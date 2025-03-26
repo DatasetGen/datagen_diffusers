47385c92b03c5e5fc592b69a6f41cc6777e1766b
josericardopenase <josericardopenase@gmail.com>
2025-03-26 10:41:01 +0000

    enable passing to cuda

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ async def dreamlike(prompt: str):
 async def flux(prompt: str):
     pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
     pipe.enable_sequential_cpu_offload()  # save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+    pipe.to("cuda")
     image = pipe(
         prompt,
         guidance_scale=0.0,
