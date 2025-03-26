0536b7969ba9acab3585bf644fe20605cb0b0fa0
josericardopenase <josericardopenase@gmail.com>
2025-03-26 10:16:22 +0000

    enable sequential cpu offload

--- main.py
+++ main.py
@@ -44,8 +44,7 @@ async def dreamlike(prompt: str):
 @app.get("/flux", response_class=Response)
 async def flux(prompt: str):
     pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
-    pipe.to("cuda")
-    pipe.enable_model_cpu_offload()  # save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+    pipe.enable_sequential_cpu_offload()  # save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
     image = pipe(
         prompt,
         guidance_scale=0.0,
